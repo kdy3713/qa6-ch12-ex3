79ae505ab8558f42f8bfe389af4d368f8320dfcc
kdy3713-commits <kdy3713@gmail.com>
2026-08-04 17:52:40 +0900

    업데이트

--- PR_test_2or5.py
+++ PR_test_2or5.py
@@ -1,12 +1,25 @@
 # 1부터 100까지 2와 5의배수 확인
 for i in range(1, 101):
-    if i % 2 == 0 or i % 5 == 0:
-        # 2의 배수이거나 5의 배수인 경우 (10의 배수는 둘 다 해당)
-        if i % 2 == 0 and i % 5 == 0:
-            print("2와 5의 배수입니다")
-        elif i % 2 == 0:
-            print("2의 배수입니다")
+    print(i)
+    if i % 2 == 0 and i % 5 == 0:
+        print("2와 5의 배수입니다")
+    elif i % 2 == 0:
+        print("2의 배수입니다")
+    elif i % 5 == 0:
+        print("5의 배수입니다")
+
+
+
+    # 100부터 1까지 -1씩 감소
+    for i in range(100, 0, -1):
+        num = str(i)
+        
+        # 3, 6, 9 개수 카운트
+        count = num.count('3') + num.count('6') + num.count('9')
+        
+        # 3, 6, 9가 들어있으면 개수만큼 '짝' 출력
+        if count > 0:
+            print(f"{i} {'짝' * count}")
+        # 없으면 숫자만 출력
         else:
-            print("5의 배수입니다")
-    else:
-        print("계산완료")
+            print(i)
